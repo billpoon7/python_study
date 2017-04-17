d8179a0db734c42501cce8cdbd2fd528679b6187
billpoon <pan.quanbiao@active.ai>
2017-04-17 15:52:13 +0800

    add a recursive method

--- function.py
+++ function.py
@@ -1,32 +1,45 @@
 def my_abs(x):
-	if x > 0 :
-		return x
-	else :
-		return -x
+    if x > 0:
+        return x
+    else:
+        return -x
+
+# print(my_abs(-9))
+
 
 def power(x, n=2):
-	s = 1
-	while n > 0:
-		n = n - 1
-		s = s * x
-	return s
+    s = 1
+    while n > 0:
+        n = n - 1
+        s = s * x
+    return s
+
+# print (power(2))
+
 
 def add_end(L=None):
-	if L is None:
-		L = []
-	L.append('END')
-	return L
+    if L is None:
+        L = []
+    L.append('END')
+    return L
 
-# 
+# print(add_end())
+
+#
+
+
 def calc(*numbers):
-	sum = 0
-	for n in numbers:
-		sum = sum + n
-	return sum
+    sum = 0
+    for n in numbers:
+        sum = sum + n
+    return sum
+
+nums = [1, 2, 3, 4]
 # print(calc(*nums))
 
+
 def person(name, age, **kw):
-	print('name: ', name, 'age: ', 'other:', kw)
+    print('name: ', name, 'age: ', 'other:', kw)
 
 # person('billpoon', 27, city='zhongshan')
 
@@ -34,7 +47,6 @@ extra = {'city': 'Beijing', 'job': 'Engineer'}
 # person('Jack', 24, city=extra['city'], job=extra['job'])
 # person('Jack', 24, **extra)
 
-nums = [1, 2, 3, 4]
 
 def person_check(name, age, **kw):
     if 'city' in kw:
@@ -45,12 +57,12 @@ def person_check(name, age, **kw):
         pass
     print('name:', name, 'age:', age, 'other:', kw)
 
-person_check('Jack', 24, city='Beijing', addr='Chaoyang', zipcode=123456)
-
-# print(my_abs(-9))
-
-# print (power(2))
-
-# print(add_end())
+# person_check('Jack', 24, city='Beijing', addr='Chaoyang', zipcode=123456)
 
 
+def fact(n):
+    if n == 1:
+        return 1
+    return n * fact(n - 1)
+
+# print(fact(5))
